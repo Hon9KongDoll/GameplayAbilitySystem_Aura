760a55e39b1e800c6c43157a692f549982450b21
Hon9KongDoll <1483033869@qq.com>
2024-11-27 00:03:15 +0800

    AuraAssets AuraCharacter

---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+// Abstract:阻止用户在虚幻编辑器中向这个世界中添加这个类的Actor,或者在游戏过程中创建这个类的实例
+UCLASS(Abstract)
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+};
