88d7fcb668afdd684122628d3830745b2ddd8ccd
Huizhang <1483033869@qq.com>
2025-04-23 22:45:46 +0800

    The Base Character Class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,12 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
