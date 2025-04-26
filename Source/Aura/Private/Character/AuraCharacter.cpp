1ffec2a7e2fb0aeda6b1bcad497809d2d667dd8a
Huizhang <1483033869@qq.com>
2025-04-26 19:45:53 +0800

    Player normal play

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -1,1 +1,21 @@
 #include "Character/AuraCharacter.h"
+
+// Engine
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// 角色的朝向自动匹配移动方向
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// 旋转速率
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	// 限制角色或物体的移动在特定的平面上
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// 在游戏开始时自动将对象对齐到约束平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 注 ：这里写错会导致角色朝向无法成功匹配到移动方向
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -5,9 +5,11 @@ AAuraCharacterBase::AAuraCharacterBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	LeftWeapon = CreateDefaultSubobject<UStaticMeshComponent>(FName("LeftWeapon"));
+	LeftWeapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	LeftWeapon->SetupAttachment(GetMesh(), FName("Weapon_L"));
 
 	RightWeapon = CreateDefaultSubobject<UStaticMeshComponent>(FName("RightWeapon"));
+	RightWeapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RightWeapon->SetupAttachment(GetMesh(), FName("Weapon_R"));
 }
 
