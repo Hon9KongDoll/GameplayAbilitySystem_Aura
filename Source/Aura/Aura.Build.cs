6d4195bdb9466305154c5c1d52e414b1eec369d3
Hon9KongDoll <1483033869@qq.com>
2026-02-01 20:22:03 +0800

    Ability System Compomemt and Attribute Set

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -7,8 +7,17 @@ public class Aura : ModuleRules
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+            "GameplayAbilities",
+            "GameplayTasks",
+            "GameplayTags"
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
